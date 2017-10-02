4705fa4101ef723af0bd9022cac321e023573076
Nguyễn Trần Dũng <trandung1128@gmail.com>
2017-10-02 17:52:34 +0700

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 var _ = require('lodash')
 var listLocation = [];
 io.on("connection", function (socket) {
