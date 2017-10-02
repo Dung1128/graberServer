c6564b6c38fe325ad199b5ed4248bb122ac9a2fb
Nguyễn Trần Dũng <trandung1128@gmail.com>
2017-10-02 16:29:47 +0700

    create server socket

---
+++ Server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+var _ = require('lodash')
+var listLocation = [];
+io.on("connection", function (socket) {
+  console.log("user connected" + socket.id);
+  io.sockets.emit('socket-id', socket.id);
+  socket.on('location-client', function (data) {
+    // listLocation.push(data);
+    var flag = 0;
+    if (listLocation.length == 0) {
+      listLocation.push(data);
+    } else {
+      var result = _.findIndex(listLocation, item => {
+        console.log('find', item.key, socket.id);
+        return item.key === socket.id;
+      });
+      console.log(result);
+      if (result >= 0) {
+        listLocation[result].data = data.data
+      } else {
+        listLocation.push(data);
+      }
+
+    }
+
+    console.log('location: ', listLocation);
+    io.sockets.emit('send-location', listLocation);
+  })
+
+});
